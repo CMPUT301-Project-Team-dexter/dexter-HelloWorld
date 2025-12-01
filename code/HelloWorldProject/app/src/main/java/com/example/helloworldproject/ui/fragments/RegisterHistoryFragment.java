d5d11dbeeb4d9e167580f68abd93aea79c44eec7
Hualin457 <hualin@ualberta.ca>
2025-12-01 12:50:53 -0700

    small optimization on UI

--- code/HelloWorldProject/app/src/main/java/com/example/helloworldproject/ui/fragments/RegisterHistoryFragment.java
+++ code/HelloWorldProject/app/src/main/java/com/example/helloworldproject/ui/fragments/RegisterHistoryFragment.java
@@ -2,21 +2,15 @@ package com.example.helloworldproject.ui.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuProvider;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Lifecycle;
 
 import com.example.helloworldproject.R;
 import com.example.helloworldproject.data.EventRepository;
@@ -24,13 +18,11 @@ import com.example.helloworldproject.databinding.FragHomeEventListBinding;
 import com.example.helloworldproject.model.Event;
 import com.example.helloworldproject.ui.activities.event.EventDetailActivity;
 import com.example.helloworldproject.ui.utils.EventCardAdapter;
-import com.google.android.material.appbar.MaterialToolbar;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RegisterHistoryFragment extends Fragment {
-    ActivityResultLauncher<String> requestCamera = null;
     FragHomeEventListBinding binding;
     ArrayList<Event> eventListBackEnd = new ArrayList<>();
     private EventCardAdapter adapter;
@@ -50,30 +42,17 @@ public class RegisterHistoryFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        MaterialToolbar toolbar = view.findViewById(R.id.toolbar);
-        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
-        requireActivity().addMenuProvider(new MenuProvider() {
-            @Override
-            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
-                menuInflater.inflate(R.menu.home_event_list_menu, menu);
-            }
-
-            @Override
-            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
-                // TODO: implement filter function later
-                if (menuItem.getItemId() == R.id.home_filter_button) {
-                    // filter not implemented yet
-                    return true;
-                }
-                return false;
-            }
-        }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(binding.toolbar);
 
         // Top bar title
         ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayShowTitleEnabled(true);
             actionBar.setTitle("Register History");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            binding.toolbar.setNavigationOnClickListener(
+                v -> requireActivity().getOnBackPressedDispatcher().onBackPressed()
+            );
         }
 
         adapter = new EventCardAdapter(requireContext(), eventListBackEnd);
@@ -84,9 +63,9 @@ public class RegisterHistoryFragment extends Fragment {
                 startActivity(EventDetailActivity.newIntent(requireActivity(), e.getId()));
             } else {
                 Toast.makeText(
-                        requireContext(),
-                        "Error loading event details: event is null",
-                        Toast.LENGTH_SHORT
+                    requireContext(),
+                    "Error loading event details: event is null",
+                    Toast.LENGTH_SHORT
                 ).show();
             }
         });
@@ -102,28 +81,27 @@ public class RegisterHistoryFragment extends Fragment {
     }
 
     private void refreshEventList() {
-        // TODO: add a loading spinner here?
-            loadJoinedEvents();
+        loadJoinedEvents();
     }
 
     // Entrant view: only joinable events
     private void loadJoinedEvents() {
         EventRepository.INSTANCE.loadRegisterHistoryEvents(
-                new EventRepository.ListCallback() {
-                    @Override
-                    public void onLoaded(List<Event> events) {
-                        updateAdapterFrom(events);
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                        Toast.makeText(
-                                requireContext(),
-                                "Failed to load events: " + e.getMessage(),
-                                Toast.LENGTH_LONG
-                        ).show();
-                    }
+            new EventRepository.ListCallback() {
+                @Override
+                public void onLoaded(List<Event> events) {
+                    updateAdapterFrom(events);
                 }
+
+                @Override
+                public void onError(Exception e) {
+                    Toast.makeText(
+                        requireContext(),
+                        "Failed to load events: " + e.getMessage(),
+                        Toast.LENGTH_LONG
+                    ).show();
+                }
+            }
         );
     }
 }
